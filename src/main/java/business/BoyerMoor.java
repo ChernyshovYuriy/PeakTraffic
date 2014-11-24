26614709e6c42a0051938afdee05d430bc574a12
YuriyChernyshov <chernyshov.yuriy@gmail.com>
2014-11-24 22:07:47 +0200

    Add BoyerMoor algorithm. Solve the problem.

---
+++ src/main/java/business/BoyerMoor.java
@@ -0,0 +1,108 @@
+package business;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chernyshovyuriy
+ * Date: 11/9/13
+ * Time: 6:21 PM
+ */
+public class BoyerMoor {
+
+    /**
+     * Returns the index within this string of the first occurrence of the
+     * specified substring. If it is not a substring, return -1.
+     *
+     * @param haystack The string to be scanned
+     * @param needle   The target string to search
+     * @return The start index of the substring
+     */
+    public static /*List<Integer>*/int indexOf(char[] haystack, char[] needle) {
+        //List<Integer> result = new ArrayList<Integer>();
+        int result = 0;
+        if (needle.length == 0) {
+            //result.add(0);
+            result = 0;
+            return result;
+        }
+        int charTable[] = makeCharTable(needle);
+        int offsetTable[] = makeOffsetTable(needle);
+        for (int i = needle.length - 1, j; i < haystack.length; ) {
+            for (j = needle.length - 1; needle[j] == haystack[i]; --i, --j) {
+                if (j == 0) {
+                    // To return the first matched index
+                    //return i;
+                    //result.add(i);
+                    result++;
+                    break;
+                }
+            }
+            // i += needle.length - j; // For naive method
+            i += Math.max(offsetTable[needle.length - 1 - j], charTable[haystack[i]]);
+        }
+        /*if (result.isEmpty()) {
+            result.add(-1);
+        }*/
+        if (result == 0) {
+            result = -1;
+        }
+        return result;
+    }
+
+    /**
+     * Makes the jump table based on the mismatched character information.
+     */
+    private static int[] makeCharTable(char[] needle) {
+        final int ALPHABET_SIZE = 256;
+        int[] table = new int[ALPHABET_SIZE];
+        for (int i = 0; i < table.length; ++i) {
+            table[i] = needle.length;
+        }
+        for (int i = 0; i < needle.length - 1; ++i) {
+            table[needle[i]] = needle.length - 1 - i;
+        }
+        return table;
+    }
+
+    /**
+     * Makes the jump table based on the scan offset which mismatch occurs.
+     */
+    private static int[] makeOffsetTable(char[] needle) {
+        int[] table = new int[needle.length];
+        int lastPrefixPosition = needle.length;
+        for (int i = needle.length - 1; i >= 0; --i) {
+            if (isPrefix(needle, i + 1)) {
+                lastPrefixPosition = i + 1;
+            }
+            table[needle.length - 1 - i] = lastPrefixPosition - i + needle.length - 1;
+        }
+        for (int i = 0; i < needle.length - 1; ++i) {
+            int slen = suffixLength(needle, i);
+            table[slen] = needle.length - 1 - i + slen;
+        }
+        return table;
+    }
+
+    /**
+     * Is needle[p:end] a prefix of needle?
+     */
+    private static boolean isPrefix(char[] needle, int p) {
+        for (int i = p, j = 0; i < needle.length; ++i, ++j) {
+            if (needle[i] != needle[j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns the maximum length of the substring ends at p and is a suffix.
+     */
+    private static int suffixLength(char[] needle, int p) {
+        int len = 0;
+        for (int i = p, j = needle.length - 1;
+             i >= 0 && needle[i] == needle[j]; --i, --j) {
+            len += 1;
+        }
+        return len;
+    }
+}
--- src/main/java/business/PeakTrafficCounter.java
+++ src/main/java/business/PeakTrafficCounter.java
@@ -11,23 +11,29 @@ import java.util.*;
  * Date: 24.11.14
  * Time: 14:13
  */
+
+/**
+ * {@link business.PeakTrafficCounter} is the main logic holder. It performs peak traffic calculation.
+ */
 public class PeakTrafficCounter implements LogReaderListener {
 
     /**
      * This map holds association between concrete user (as users email) and all corresponded emails that are belongs
      * to this user. Corresponded emails are stored in the set to avoid duplicates.
      */
-    private final Map<String, Set<String>> dataMap = new TreeMap<String, Set<String>>();
+    private final Map<String, TreeSet<String>> dataMap = new TreeMap<String, TreeSet<String>>();
 
     @Override
     public void processLogRecord(final LogEntity record) {
 
         // Get set of the emails associated with concrete user
-        Set<String> usersSet = dataMap.get(record.getUser());
+        TreeSet<String> usersSet = dataMap.get(record.getUser());
 
         // If there is none - create it
         if (usersSet == null) {
-            usersSet = new TreeSet<String>();
+            // Apply custom comparator in order to compare users emails as strings
+            usersSet = new TreeSet<String>(new EmailComparator());
+            usersSet.add(record.getUser());
 
             dataMap.put(record.getUser(), usersSet);
         }
@@ -38,8 +44,70 @@ public class PeakTrafficCounter implements LogReaderListener {
 
     @Override
     public void onProcessComplete() {
-        for (String user : dataMap.keySet()) {
-            Logger.printMessage("User:" + user + " Data:" + dataMap.get(user));
+        // Set of the final clusters which will be printed
+        final Set<String> finalResult = new HashSet<String>();
+        // Set of the general data keys (collected at the 'processLogRecord' callback)
+        final Set<String> dataMapSet = dataMap.keySet();
+        // Single cluster as set of the
+        Set<String> cluster;
+        // Current user email as array of the chars
+        char[] userAsChars;
+        // Sub-user (the one who receives data from the user) email as array of the chars
+        char[] subUserAsChars;
+        // Current user email as string
+        String userAsString;
+        // Cluster length
+        int userSetLength;
+        // Result of the Boyer-Moor algorithm
+        int result;
+
+        // Iterate over set
+        for (String user : dataMapSet) {
+            // get current cluster
+            cluster = dataMap.get(user);
+            // get size of the current cluster
+            userSetLength = cluster.size();
+            // convert current cluster to string
+            userAsString = cluster.toString();
+            // convert current cluster to chars array
+            userAsChars = userAsString.toCharArray();
+
+            result = -1;
+
+            // Iterate over sub-users
+            for (String sub_user : dataMapSet) {
+                // Skip if loop meet same user
+                if (sub_user.equals(user)) {
+                    continue;
+                }
+                // convert sub- cluster to chars array
+                subUserAsChars = dataMap.get(sub_user).toString().toCharArray();
+
+                // Run Boyer-Moor algorithm
+                result = BoyerMoor.indexOf(userAsChars, subUserAsChars);
+                if (result != -1) {
+                    break;
+                }
+            }
+
+            // If we pass criteria - add cluster to final set
+            if (result != -1 && userSetLength >= 3) {
+                finalResult.add(userAsString);
+            }
+        }
+        for (String value : finalResult) {
+            Logger.printMessage(value);
+        }
+    }
+
+    /**
+     * {@link business.PeakTrafficCounter.EmailComparator} is a helper class which performs email natural compare.
+     */
+    private class EmailComparator implements Comparator<String> {
+
+        @Override
+        public int compare(final String str1, final String str2) {
+            return str1.compareTo(str2);
         }
     }
 }
